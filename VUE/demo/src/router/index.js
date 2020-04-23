4d3cf662408948360071304255ac5b716c56dee4
sunshine <877430318@qq.com>
2020-04-23 08:05:57 +0800

    修改

--- VUE/demo/src/router/index.js
+++ VUE/demo/src/router/index.js
@@ -1,49 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+import routes from './router.js'
 
 Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
-  },
-
-  // 原生vue
-  {
-    path: '/original',
-    name: 'original',
-    component: () => import('../views/Original/index.vue'),
-    children: [
-      {
-        path: '/original/sidebar',
-        name: 'ogSidebar',
-        component: () => import('../views/Original/sidebar/index.vue')
-      }
-    ]
-  },
-
-  // element-ui
-  {
-    path: '/elementuilayout',
-    name: 'elLayout',
-    component: () => import('../views/ElementUi/index.vue'),
-    children: [
-      {
-        path: '/elementuilayout/date',
-        name: 'elDate',
-        component: () => import('../views/ElementUi/Date/index.vue')
-      }
-    ]
-  }
-]
 
 const router = new VueRouter({
   mode: 'history',
